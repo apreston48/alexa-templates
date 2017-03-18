8400165543ffc017de61dedf565d12306c603a92
apreston48 <apreston48@gmail.com>
2017-03-17 23:07:16 -0400

    reference to ffmpeg

--- random-audio/index.js
+++ random-audio/index.js
@@ -1,5 +1,8 @@
 // this code sample will play random audio files stored in the audioFiles variable
 
+// use ffmpeg to convert audio
+// ffmpeg -i <input-file> -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 <output-file.mp3>
+
 var audioFiles = ["<audio src='https://s3.amazonaws.com/my-ssml-samples/Flourish.mp3' />","<audio src='https://s3.amazonaws.com/my-ssml-samples/cheap_thrills.mp3' />","<audio src='https://s3.amazonaws.com/my-ssml-samples/Flourish.mp3' />"];  // Array of items
 
 var Alexa = require('alexa-sdk');
@@ -7,7 +10,7 @@ var Alexa = require('alexa-sdk');
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
 
-    // exa.appId = 'amzn1.ask.skill.1234';
+    // alexa.appId = 'amzn1.ask.skill.1234';
 
     alexa.registerHandlers(handlers);
     alexa.execute();
